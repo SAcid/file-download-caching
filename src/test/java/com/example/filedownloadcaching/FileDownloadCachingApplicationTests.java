9dac805b1f67592603c6b5c5b9f9324146d44aad
이성면 <shipistz@gmail.com>
2022-07-02 19:50:10 +0900

    init

---
+++ src/test/java/com/example/filedownloadcaching/FileDownloadCachingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.filedownloadcaching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileDownloadCachingApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
